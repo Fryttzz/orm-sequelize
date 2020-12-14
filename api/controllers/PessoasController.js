397c14459d26941d51af8062246a649045875b84
Matheus Santos <matheusps0012@gmail.com>
2020-12-14 14:33:21 -0300

    Done - Basic CRUD

--- api/controllers/PessoasController.js
+++ api/controllers/PessoasController.js
@@ -6,7 +6,63 @@ class PessoaController {
             const todasAsPessoas = await database.Pessoas.findAll()
             return res.status(200).json(todasAsPessoas)
         } catch (error) {
-           return res.status(500).json(erros.message) 
+           return res.status(500).json(error.message) 
+        }
+    }
+
+    static async pegaUmaPessoa(req, res) {
+        const {id} = req.params
+        try {
+            const umaPessoa = await database.Pessoas.findOne({
+                where: {
+                    id: Number(id)
+                }
+            })
+            return res.status(200).json(umaPessoa)
+        } catch (error) {
+            return res.status(500).json(error.message) 
+        }
+    }
+
+    static async criaPessoa(req, res) {
+        const novaPessoa = req.body
+
+        try {
+            const novaPessoaCriada = await database.Pessoas.create(novaPessoa)
+            return res.status(201).json(novaPessoaCriada)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async atualizaPessoa(req, res) {
+        const {id} = req.params
+        const novasInfos = req.body
+
+        try {
+            await database.Pessoas.update(novasInfos, {where: {id: Number(id)}})
+
+            const pessoaAtualizada = await database.Pessoas.findOne(
+                {where: {id: Number(id)}})
+            return res.status(200).json(pessoaAtualizada)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async removePessoa(req, res) {
+        const {id} = req.params
+
+        try {
+            await database.Pessoas.destroy({
+                where: {
+                    id: Number(id)
+                }
+            })
+
+            return res.status(204).send()
+        } catch (error) {
+            return res.status(500).json(error.message)
         }
     }
 }
