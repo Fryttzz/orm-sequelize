c40015857c25585efe326ea24686ec6d7d8598b7
fryttzz <matheusps0012@gmail.com>
2020-12-19 21:54:59 -0300

    Done - DeletedAt Softdelete

--- api/seeders/20201216133007-demo-matriculas.js
+++ api/seeders/20201216133007-demo-matriculas.js
@@ -4,42 +4,42 @@ module.exports = {
 			{
 				status: "confirmado",
 				estudante_id: 1,
-				turma_id: 13,
+				turma_id: 5,
 				createdAt: new Date(),
 				updatedAt: new Date()
 		},
 		{
 			status: "confirmado",
 			estudante_id: 2,
-			turma_id: 13,
+			turma_id: 5,
 			createdAt: new Date(),
 			updatedAt: new Date()
 	},
 		{
 			status: "confirmado",
 			estudante_id: 3,
-			turma_id: 14,
+			turma_id: 6,
 			createdAt: new Date(),
 			updatedAt: new Date()
 	},
 		{
 			status: "confirmado",
 			estudante_id: 4,
-			turma_id: 15,
+			turma_id: 7,
 			createdAt: new Date(),
 			updatedAt: new Date()
 	},
 		{
 			status: "cancelado",
 			estudante_id: 1,
-			turma_id: 14,
+			turma_id: 6,
 			createdAt: new Date(),
 			updatedAt: new Date()
 	},
 		{
 			status: "cancelado",
 			estudante_id: 2,
-			turma_id: 14,
+			turma_id: 6,
 			createdAt: new Date(),
 			updatedAt: new Date()
 		}
