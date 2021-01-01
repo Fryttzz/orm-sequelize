910919e9f1d1cf0c4a92af1a331a9162714057a4
fryttzz <matheusps0012@gmail.com>
2021-01-01 15:02:21 -0300

    Done - Transações

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -3,34 +3,27 @@ const PessoaController = require('../controllers/PessoasController')
 
 const router = Router()
 
-router.get('/pessoas', PessoaController.pegaPessoasAtivas)
-
-router.get('/pessoas/todos', PessoaController.pegaTodasAsPessoa)
-
-router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
-
-router.get('/pessoas/:estudanteId/matricula', PessoaController.pegaMatriculas)
-
-router.get('/pessoas/matricula/:turmaId/confirmadas', PessoaController.pegaMatriculasPorTurma)
-
-router.get('/pessoas/matricula/lotada', PessoaController.pegaTurmaLotadas)
-
-router.post('/pessoas', PessoaController.criaPessoa)
-
-router.put('/pessoas/:id', PessoaController.atualizaPessoa)
-
-router.delete('/pessoas/:id', PessoaController.removePessoa)
-
-router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegarUmaMatricula)
-
-router.post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
-
-router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula)
-
-router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.removeMatricula)
-
-router.post('/pessoas/:id/restaura', PessoaController.restautaPessoa)
-
-router.post('/pessoas/:estudanteId/matricula/:matriculaId/restaura', PessoaController.restauraMatricula)
+router
+    .get('/pessoas', PessoaController.pegaPessoasAtivas)
+    .get('/pessoas/todos', PessoaController.pegaTodasAsPessoa)
+    
+    .get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+    .get('/pessoas/:estudanteId/matricula', PessoaController.pegaMatriculas)
+    .get('/pessoas/matricula/:turmaId/confirmadas', PessoaController.pegaMatriculasPorTurma)
+    .get('/pessoas/matricula/lotada', PessoaController.pegaTurmaLotadas)
+    .get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegarUmaMatricula)
+    
+    .post('/pessoas', PessoaController.criaPessoa)
+    .post('/pessoas/:estudanteId/cancela', PessoaController.cancelaPessoa)
+    .post('/pessoas/:id/restaura', PessoaController.restautaPessoa)
+    
+    .post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
+    .post('/pessoas/:estudanteId/matricula/:matriculaId/restaura', PessoaController.restauraMatricula)
+    
+    .put('/pessoas/:id', PessoaController.atualizaPessoa)
+    .put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula)
+    
+    .delete('/pessoas/:id', PessoaController.removePessoa)
+    .delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.removeMatricula)
 
 module.exports = router
